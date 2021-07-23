b5daadab909bfb4a8cfc3ed0b106c6238630ac76
htakahashi15 <htakahashi15@gmail.com>
2021-07-23 21:31:18 +0900

    1st commit

---
+++ db/init/02_insert_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO books VALUES (1, 'プログラミング言語C', current_timestamp);
+INSERT INTO books VALUES (2, 'やさしいコンピュータ科学', current_timestamp);
+INSERT INTO books VALUES (3, 'ゲーデル、エッシャー、バッハ―あるいは不思議の環', current_timestamp);
+INSERT INTO books VALUES (4, 'Tex ブック コンピュータによる組版システム', current_timestamp);
+INSERT INTO books VALUES (5, '人間の神話 狼人間を撃つ銀の弾はない', current_timestamp);
